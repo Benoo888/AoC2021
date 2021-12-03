1f1dd7c15aca71ba19436b97e8dba5776483a31d
Menno Turksma <mennoturksma@dsw.nl>
2021-12-03 14:45:11 +0100

    enum lolol

--- AoC2021/Solutions/Day03/Part02.cs
+++ AoC2021/Solutions/Day03/Part02.cs
@@ -8,8 +8,8 @@
 
             var intArray = input.Select(input => input.Select(x => (int)char.GetNumericValue(x)).ToArray());
 
-            var OxygenRate = GetSingleRate(intArray.ToList(), 1, true);
-            var Co2Rate = GetSingleRate(intArray.ToList(), 0, false);
+            var OxygenRate = GetSingleRate(intArray.ToList(), 1, Preference.MostCommon);
+            var Co2Rate = GetSingleRate(intArray.ToList(), 0, Preference.LeastCommon);
 
             var oxygenValue = Convert.ToInt32(string.Join("", OxygenRate.Select(x => x.ToString())), 2);
             var co2Value = Convert.ToInt32(string.Join("", Co2Rate.Select(x => x.ToString())), 2);
@@ -17,25 +17,25 @@
             PrettyConsolewriter.WriteLine($"Day 3 part 2: {oxygenValue * co2Value} oxygen rate times co2 rate");
         }
 
-        private static int[] GetSingleRate(List<int[]> array, int preferredBit, bool mostCommonValue)
+        private static int[] GetSingleRate(List<int[]> array, int preferredBit, Preference preference)
         {
             for (int i = 0; i < array.First().Length; i++)
             {
                 if (array.Count == 1) break;
-                array = GetRates(array, i, preferredBit, mostCommonValue);
+                array = GetRates(array, i, preferredBit, preference);
             }
 
             return array.Single();
         }
 
-        private static List<int[]> GetRates(List<int[]> array, int index, int preferredBit, bool mostCommonValue)
+        private static List<int[]> GetRates(List<int[]> array, int index, int preferredBit, Preference preference)
         {
             var digit = array.Select(item => item[index]);
             var digitPreferred = digit.Where(d => d == preferredBit).Count();
             var digitSecondary = digit.Where(d => d != preferredBit).Count();
 
-            if ((mostCommonValue && digitPreferred >= digitSecondary) ||
-              (!mostCommonValue && digitPreferred <= digitSecondary))
+            if ((preference == Preference.MostCommon && digitPreferred >= digitSecondary) ||
+              (preference == Preference.LeastCommon && digitPreferred <= digitSecondary))
             {
                 return array.Select(item => item).Where(b => b[index] == preferredBit).ToList();
             }
@@ -43,6 +43,12 @@
             {
                 return array.Select(item => item).Where(b => b[index] != preferredBit).ToList();
             }
+        }
+
+        private enum Preference
+        {
+            MostCommon,
+            LeastCommon
         }
     }
 }
